1287f0bbb62cfd7e2c204f880a11affa202f9e0f
lyj00912 <3122742760@qq.com>
2024-04-11 20:57:15 +0800

    reverted changes to commit d913a2b682b172a232ba80c12dc0d6da2711aa2f, since SD.h is a necessity and cannot be un-included. now we're ready to find the next "FAT DUDE".

--- CompilationHelper.cpp
+++ CompilationHelper.cpp
@@ -6,14 +6,14 @@
 // Side Effect:
 // Whatever .cpp included here will have a slight folder confusion that is needed to fixed manually.
 
-//#include "GraphicsDriver/DEV_Config.cpp"  // 7% prog, 14% ram
-#include "GraphicsDriver/LCD_Bmp.cpp"  // 22% prog, 46% ram (overweight dude)
-//#include "GraphicsDriver/LCD_Driver.cpp"  // 5% prog, 5% ram
-//#include "GraphicsDriver/LCD_GUI.cpp"  // 5% prog, 5% ram
-//#include "GraphicsDriver/LCD_Touch.cpp"  // 5% prog, 5% ram
+#include "GraphicsDriver/DEV_Config.cpp"
+#include "GraphicsDriver/LCD_Bmp.cpp"
+#include "GraphicsDriver/LCD_Driver.cpp"
+#include "GraphicsDriver/LCD_GUI.cpp"
+#include "GraphicsDriver/LCD_Touch.cpp"
 
 //#include "GraphicsDriver/font8.c"
 //#include "GraphicsDriver/font12.c"
-//#include "GraphicsDriver/font16.c"
+#include "GraphicsDriver/font16.c"
 //#include "GraphicsDriver/font20.c"
 //#include "GraphicsDriver/font24.c"
